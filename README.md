f68a85205110f6e4ec04ac38b8a8ad5434b1b02d
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-26 13:00:17 +0530

    Initial Dependency Configuration

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # We are making an AI extension that can help you solve Leetcode problems.
 
 # We will use the OpenAI API to generate code that can solve the problem.
+
+npm install --save-dev @crxjs/vite-plugin@beta
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json'; // We'll create this next
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), crx({ manifest })],
 })
