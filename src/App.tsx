dd6a9e743a093a3f816226d084ac45af37fd2819
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-28 17:11:30 +0530

    Popup Component

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ const Popup: React.FC = () => {
   const [apiKey, setApiKey] = useState<string>("");
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+
   //we need to store the api key in the chrome browser local storage
-  const saveApiKey = () => {};
+  const saveApiKey = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    try {
+    } catch {}
+  };
   //when we load the popup we need to check if the api key is stored in the local storage
   useEffect(() => {}, []);
 
