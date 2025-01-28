c4e72d88edf5d43d0a7e0e484832877d1ede6ce0
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-01-28 13:32:17 +0530

    Popup Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-function App() {
-  return (
-    <>
-      <div>
-        <h1>Hello</h1>
-      </div>
-    </>
-  );
-}
+import React, { useEffect, useState } from "react";
 
-export default App;
+const Popup: React.FC = () => {
+  const [apiKey, setApiKey] = useState<string>("");
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  //we need to store the api key in the chrome browers local storage
+  const saveApiKey = () => {};
+  //when we load the popup we need to check if the api key is stored in the local storage
+  useEffect(() => {}, []);
+
+  return <div>Popup</div>;
+};
+
+export default Popup;
